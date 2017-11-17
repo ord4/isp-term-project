24707f3330bb24a4f6654070444a98f40eb576b9
Orion Davis <ord4@zips.uakron.edu>
2017-11-17 18:08:43 -0500

    Query statement written, now to try and route on match.

--- src/index.php
+++ src/index.php
@@ -5,7 +5,7 @@
     <form id="login_form">
         <input type="text" id="username" placeholder="Username" required>
         <input type="password" id="password" placeholder="Password" required>
-        <button type="button" id="submit_creds">Submit</button>
+        <input type="submit" name="submit_creds" value="Log in">
     </form>
 
     <?php
@@ -20,6 +20,20 @@
 
         if($conn->connect_error) {
             die("Connection failed. ". $conn->connect_error);
+        }
+
+        // Get the credentials from the form
+        if (isset($_POST['submit_creds'])) {
+            print("<p>Inside post method?</p>");
+            $user_username = $_POST['username'];
+            $user_password = $_POST['password'];
+
+            $query_str = "SELECT * FROM user WHERE username=$user_username AND password=$user_password";
+            $result = mysqli_query($conn, $query_str);
+
+            if ($result) {
+                print("<p>Matched the query");
+            }
         }
     ?>
 </body>
