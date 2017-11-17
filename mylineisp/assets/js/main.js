ee9374b999071d081e29c1e7f322adcf1ed30e0c
Tim Snyder <31523862+timothyCSnyder@users.noreply.github.com>
2017-11-16 21:25:11 -0500

    Documentation

--- mylineisp/assets/js/main.js
+++ mylineisp/assets/js/main.js
@@ -1,24 +1,39 @@
+
+// Seriazlie timeline into javascript
 function jsonifyTimeline() {
 
+        // Init JSON object
         var timelineJSON = {};
+
         var timelineTitle = document.getElementById("timeline-title").textContent;
         timelineJSON["timeline-title"] = timelineTitle;
+
+        // Init empty array in object for nodes
         timelineJSON["nodes"] = [];
 
+        // JS array holding timeline nodes
         var nodes = document.getElementsByClassName("node");
 
+        // Iterate over every node in array
         for(i = 0; i < nodes.length; i++){
-            children = nodes[i].children;
+
+            // JS object holding the children of current node
+            var children = nodes[i].children;
+
             var name = children[0].textContent;
             var date = children[1].textContent;
+
             var node = {"node-title" : name, "node-date" : date};
+
             timelineJSON["nodes"].push(node);
         }
 
+        // output to page
     var jsonout = document.getElementById("json-out");
     jsonout.innerHTML=JSON.stringify(timelineJSON);
  }
 
+// Add a new node to the bottom of the timeline... no user input yet
  function addNode(){
     var i = document.getElementsByClassName("node").length + 1;
 
@@ -40,3 +55,16 @@ function jsonifyTimeline() {
 
     document.getElementById("nodes").appendChild(newNode);
  }
+
+
+// JQuery functions for Drag-And-Drop interaction
+$( function() {
+    $( "#nodes" ).sortable();
+    $( "#nodes" ).disableSelection();
+    $( "#nodes" ).draggable();
+    $('#trash').droppable({
+        drop: function(event, ui) {
+            ui.draggable.remove();
+        }
+    });
+} );
