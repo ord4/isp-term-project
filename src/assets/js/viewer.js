d335ee2bd7c67992f31d838f077c45fe5581243f
Tim <tcs49@zips.uakron,.edu>
2017-11-24 17:34:06 -0500

    ui on viewer

--- src/assets/js/viewer.js
+++ src/assets/js/viewer.js
@@ -3,13 +3,35 @@ timeline["nodes"] = [];
 
 
 function loadTimeline(){
-    var timelineJSON = "{\"theme\":\"#1861bb\",\"title\":\"CppCon 2017 Program\",\"desc\":\"CppCon is the annual, week-long face-to-face gathering for the entire C++ community. \",\"nodes\":[{\"node-title\":\"Lightning Talks\",\"node-date\":\"Saturday, September 23\",\"node-more\":[{\"node-text\":\"15 minute presentations from top C++ professionals.\"},{\"node-image\":\"http:\/\/clipart-library.com\/images\/8cAbM6zLi.jpg\"}]},{\"node-title\":\"Concurrent Thinking in C++\",\"node-date\":\"Sunday, September 24\",\"node-more\":[{\"node-text\":\"A two day training course taught by Anthony Williams of Just Software Solutions Ltd.\"},{\"node-link\":\"https:\/\/cppcon2017.sched.com\/event\/Bhaj\/concurrent-thinking-in-c\"}]},{\"node-title\":\"Learning and Teaching Modern C++\",\"node-date\":\"Monday, September 25\",\"node-more\":[{\"node-text\":\"The basics of Modern C++ and their applications in the classroom.\"},{\"node-link\":\"http:\/\/en.cppreference.com\/w\/\"}]}]}";
+    var timelineJSON = "{\"theme\":\"red\",\"title\":\"CppCon 2017 Program\",\"desc\":\"CppCon is the annual, week-long face-to-face gathering for the entire C++ community. \",\"nodes\":[{\"node-title\":\"Lightning Talks\",\"node-date\":\"Saturday, September 23\",\"node-more\":[{\"node-text\":\"15 minute presentations from top C++ professionals.\"},{\"node-image\":\"http:\/\/clipart-library.com\/images\/8cAbM6zLi.jpg\"}]},{\"node-title\":\"Concurrent Thinking in C++\",\"node-date\":\"Sunday, September 24\",\"node-more\":[{\"node-text\":\"A two day training course taught by Anthony Williams of Just Software Solutions Ltd.\"},{\"node-link\":\"https:\/\/cppcon2017.sched.com\/event\/Bhaj\/concurrent-thinking-in-c\"}]},{\"node-title\":\"Learning and Teaching Modern C++\",\"node-date\":\"Monday, September 25\",\"node-more\":[{\"node-text\":\"The basics of Modern C++ and their applications in the classroom.\"},{\"node-link\":\"http:\/\/en.cppreference.com\/w\/\"}]}]}";
     timeline = JSON.parse(timelineJSON);
-    document.getElementById("main-jumbo").style.backgroundColor = timeline["theme"];
+
     renderTimeline();
+    applyTheme();
 }
 
+function applyTheme(){
+    var theme = timeline["theme"];
+    var headColor;
+    var highlightColor;
 
+    switch(theme){
+        case "blue":
+            headColor = "#1861bb";
+            highlightColor = "#cac7f9";
+            break;
+        case "red":
+            headColor = "#bb1919";
+            highlightColor = "";
+            break;
+        case "green":
+            headColor = "#17bb66";
+            highlightColor = "";
+            break;
+    }
+
+    document.getElementById("main-jumbo").style.backgroundColor = headColor;
+}
 
 
 function renderTimeline(){
@@ -17,18 +39,11 @@ function renderTimeline(){
     document.getElementById("timeline-desc").textContent = timeline["desc"];
     
     var nodes = timeline["nodes"];
-    //document.getElementById("timeline-title").innerHTML = timeineTitle;
-
     for(var i = 0; i < nodes.length; i++){
         document.getElementById("nodes").appendChild(renderNode(nodes[i]));
     }
 
-    $( ".node-more" ).accordion({
-      active: false,
-      collapsible: true,
-      animate: 250
 
-    });
 }
 
 function renderNode(node){
@@ -62,7 +77,7 @@ function renderNode(node){
     var nodeLi = document.createElement("li");
     nodeLi.className = "node";
 
-    nodeLi.appendChild(nodeDateDiv);
+    document.getElementById("nodes").appendChild(nodeDateDiv);
     nodeLi.appendChild(nodeMoreDiv);
 
     return nodeLi;
@@ -71,25 +86,28 @@ function renderNode(node){
 function renderNodeItem(item){
 
     var type = Object.keys(item).toString();
-    
+    var itemDiv;
     switch(type){
 
         case "node-text":
-            var itemDiv = document.createElement("div");
-            itemDiv.className = type;
+            itemDiv = document.createElement("div");
             itemDiv.textContent = item[type];
             break;
 
         case "node-link":
-            var itemDiv = document.createElement("p");
-            itemDiv.textContent = "IM A LINK";
+            itemDiv = document.createElement("a");
+            itemDiv.href = item[type];
+            itemDiv.textContent = item[type];
             break;
 
         case "node-image":
-            var itemDiv = document.createElement("p");
-            itemDiv.textContent = "IM AN IMAGE";
+            itemDiv = document.createElement("img");
+            itemDiv.src = item[type];
             break;
     }
+
+
+    itemDiv.className = type;
 
     return itemDiv;
 }
@@ -99,10 +117,11 @@ function renderNodeItem(item){
 // JQuery functions for Drag-And-Drop interaction
 $(document).ready(function() {
     loadTimeline();
-    $( "#nodes" ).sortable({
-         disabled: true
+
+     $( ".node-more" ).accordion({
+      active: false,
+      collapsible: true,
+      animate: 250
+
     });
-    $( "#nodes" ).disableSelection();
-
-
 });
